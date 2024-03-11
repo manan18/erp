71fccd4c67f4729904cf1a9554e603f195ed828e
sumit-022 <lcb2021016@iiitl.ac.in>
2024-03-12 00:31:25 +0530

    fixes

--- src/components/layout/public.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AuthContext from "@/contexts/auth-context";
-
-const PublicLayout = ({ children }: { children: React.ReactNode }) => {
-  return { children };
-};
-
-export default PublicLayout;
